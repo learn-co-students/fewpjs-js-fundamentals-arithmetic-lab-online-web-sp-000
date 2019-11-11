0c3ce9b3ee2e30b8ba01766deabc7e0ca1db07d8
Jane Venter <janeanneventer@gmail.com>
2019-11-10 21:41:54 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = 20 % 8;
+let max = Math.max(20, 2, 5, 17, 19);
