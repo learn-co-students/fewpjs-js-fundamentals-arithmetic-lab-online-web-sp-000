39e9da3829f0623f7d8d33f2e5eae5b4e2303730
treuther <tracyreuther@gmail.com>
2019-12-23 07:39:52 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+multiply
+
+let random = Math.random()
+random
+
+let mod = 16 % 6
+mod
+
+let max = Math.max(5, 10, 15, 20)
+max
