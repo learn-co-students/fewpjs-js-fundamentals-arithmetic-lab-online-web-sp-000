cf38f3695b20bae301c57dda66a726b1398e1bce
stabassum <sumu1993@gmail.com>
2020-01-04 19:44:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 // Write your code here
 
-let num1 = 2;
-let num2 = 31;
+const num1 = 2;
+const num2 = 31;
 
-let multiply = num1 * num2;
+const multiply = num1 * num2;
 
-// let random = Math.parseInt(random()) > 2;
+const random = Math.floor(Math.random() * 10) + 1;
 
 // let mod = 
 
--- test/indexTest.js
+++ test/indexTest.js
@@ -9,20 +9,20 @@ describe('index.js', function() {
       expect(multiply).to.eq(62)
     });
   });
-  // describe('random', function() {
-  //   it("is generates a random integer greater than 0", function() {
-  //     expect(random).to.be.a('number')
-  //     expect(random).to.not.eq(0)
-  //   });
-  // });
-  // describe('mod', function() {
-  //   it("is generates a remainder that is equal to 4", function() {
-  //     expect(mod).to.eq(4)
-  //   });
-  // }); 
-  // describe('max', function() {
-  //   it("will return 20 as the highest number in the set", function() {
-  //     expect(max).to.eq(20)
-  //   });
-  // });
+  describe('random', function() {
+    it("is generates a random integer greater than 0", function() {
+      expect(random).to.be.a('number')
+      expect(random).to.not.eq(0)
+    });
+  });
+  describe('mod', function() {
+    it("is generates a remainder that is equal to 4", function() {
+      expect(mod).to.eq(4)
+    });
+  });
+  describe('max', function() {
+    it("will return 20 as the highest number in the set", function() {
+      expect(max).to.eq(20)
+    });
+  });
 });
