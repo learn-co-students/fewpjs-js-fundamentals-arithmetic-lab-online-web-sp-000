63b43b85a8c7608a9beb4a08078feff6a93cea54
jgrantps <jgrantps@hotmail.com>
2020-01-12 21:08:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = (num1 * num2)
+
+const random = Math.floor( Math.random() * 1000 ) + 2
+
+const mod = 16 % 6
+
+const max = Math.max( 4, 13, 1, 20 )
