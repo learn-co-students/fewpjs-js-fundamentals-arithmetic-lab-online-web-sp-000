23f8d361b04a3e571ff2c468e0fb3f60c8a9e811
cperez1012 <perezchristian1012@gmail.com>
2020-03-09 14:34:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
 
-// Write your code here
+let num2 = 2
+
+let multiply = num1 * num2
+
+let random = Math.round(Math.random() * 10)
+
+let mod = 10 % 6
+
+let max = Math.max(2, 4, 6, 8, 20)
