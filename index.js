5a225e396334b9e525d4edff81e34c9f2a80eee7
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-24 00:04:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+// an equation whose multiplied results will equal 62
+const num1 = 4
+const num2 = 15.5
+const multiply = num1 * num2
 
-// Write your code here
+// generates a random integer greater than 0
+let random = Math.random() + 1
+
+// generates a remainder that is equal to 4
+const mod = 16 % 6
+
+// will return 20 as the highest number in the set
+const max = Math.max(5, 10, 15, 20)
