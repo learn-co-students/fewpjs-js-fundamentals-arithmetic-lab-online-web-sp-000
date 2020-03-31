2f868141e831f71e5788ddc7f8510c46c5ebaa0f
Ronald Stephens <rstephens@bluemorpho.cc>
2020-03-31 15:11:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+let num1 = 0;
+let num2 = 0;
+let multiply = 62;
+let random = Math.floor(Math.random() * 10) + 1;
 
-// Write your code here
+let num3 = 40;
+let num4 = 6;
+let mod = num3 % num4
+
+let max = 0;
+
+// multiply
+num1 = Math.floor(Math.random() * 30) + 1;
+num2 = multiply - num1
+console.log(num1);
+console.log(num2);
+console.log(`num1 + num2 = ${num1 + num2}`);
+
+// random
+console.log(`random = ${random}`);
+
+// mod
+console.log(`${num4 % num3}`)
+console.log(`mod = ${mod}`)
+
+// max
+max = Math.max(20, 12, 11, 3, 7, 0.1);
+console.log(`max = ${max}`);
+
+
+
