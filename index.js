a1fa038113d027d2e6aeee04667c43c4d9116f2c
beckyphan <github email address>
2020-04-01 19:44:39 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1
+
+let mod = 9 % 5
+
+let max = Math.max(0, 10 , 20, 5)
