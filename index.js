45f3463888b91dde0f253d28b09901a07a4a49fd
Christopher Dent <christopherdent01@gmail.com>
2020-05-21 23:24:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+//Create a variable called multiply that will multiply the variables num1 and num2 whose results will equal "62".
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+
+//Create a variable called random that will generate a random integer greater than 0.
+const random = Math.round(Math.random() * 10) + 1;
+
+//Create a variable called mod set to an equation whose remainder is equal to "4".
+
+
+const mod = 15 % 11
+
+//Create a variable called max that will return "20" as the highest number in the set.
+
+const max = Math.ceil(19.6)
