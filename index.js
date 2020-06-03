40afd1ba51890cdb90d2d19d923f8f5414bd9030
juan siu <juan.siu@gmail.com>
2020-06-03 03:05:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+
+let multiply = num1 * num2;
+
+let random = 0;
+random =  Math.floor(Math.random()*10) + 1;
+
+let remainder = 16;
+let mod = 4;
+remainder %= mod;
+
+let max = Math.max(1,2,3,5,10,20);
