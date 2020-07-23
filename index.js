10c87d788ebbed7ea0fbac4b6bc72283c2a8b981
itsbino <charles.sabino@hotmail.com>
2020-07-22 23:30:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+function multiply() {
+    let num1 = 2
+    let num2 = 31
+    return num1 * num2
+}
+
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = 19 % 5
+let max = Math.max(1,5,10,20)
