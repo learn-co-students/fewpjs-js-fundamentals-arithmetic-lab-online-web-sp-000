b26970c64437964af2194efaeed79ef44312c540
Anthony Tilelli <Anthony@tilelli.me>
2020-07-23 23:12:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-
-// Write your code here
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
+const random = Math.floor( Math.random() * 50 ) + 1
+const mod = 84 % 8;
+const max = Math.max(1,2,3,20);
