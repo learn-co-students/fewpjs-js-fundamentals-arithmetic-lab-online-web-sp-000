8a207d612a2abecdf0531ad018d30cd156caf0e8
Pancho Labrador <plabradorjr@gmail.com>
2020-08-02 05:29:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 32
+const var1 = 2
+const var2 = 31
+const multiply = var1 * var2
+const random = Math.floor( Math.random() * 1000 ) + 1
+const mod = 16 % 6
+const max = Math.max( 5, 10, 15, 20 )
