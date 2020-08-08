dbc7d3aa3e23e7ec4dd1ed7f7461ea9ef61004dd
Alvin Coulson <aaacoulson@gmail.com>
2020-08-08 00:29:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply; 
+
+multiply = num1 * num2
+
+
+let random;
+random = Math.floor(Math.random() * 10) + 1;lea
+
+let mod;
+mod = 10 % 6
+
+let max;
+max = Math.max(11, 16, 20, 3, 14, 7)
