0f8fe66e34403a0c85c4549238dc2de579cc41b6
connorworthen <connorw220@protonmail.com>
2020-09-01 12:01:23 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() > 0);
+
+let mod = 18 % 7;
+
+let max = Math.max(1, 2, 3, 20);
