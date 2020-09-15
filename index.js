46999198e317c044de84fd3ddbda63926d95f06a
ramseychapin <ramsey@ramseychapin.com>
2020-09-15 14:55:18 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
-// Write your code here
+// Write your code 
+let num1 = 62;
+let num2 = 1;
+let multiply = num1*num2;
+
+let random = Math.floor(Math.random() *10) + 1;
+
+let mod = 9%5;
+
+let max = Math.max(20);
