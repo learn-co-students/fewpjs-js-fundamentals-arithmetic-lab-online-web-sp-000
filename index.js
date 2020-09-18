0e96dfe2d6af6b17ccf121de6ffa6abdf9d84000
github username <github email address>
2020-09-18 15:57:04 -0400

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,9 @@
 let num1 = 2
 let num2 = 31
 let multiply = num1 * num2
-const random = Math.random()
-let mod = 24%5
+
+const random = Math.random() //generates random int
+
+let mod = 24%5 //remainder 4 
+
 let max = Math.max(1, 2, 3, 4, 20);
