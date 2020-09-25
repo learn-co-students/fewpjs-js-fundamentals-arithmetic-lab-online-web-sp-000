aeed6c781673c30aa9b621b8ff1e54419766b8e6
rachavez1974 <rachavez1974@gmail.com>
2020-09-25 14:31:06 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+var num1 = 31
+var num2 = 2
+
+var multiply = num1 * num2
+
+var random = Math.random()
+
+var mod = 100 % 96
+
+var max = Math.max(1, 3, 20, 5, 6, 8, 10)
