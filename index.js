d16d15ca29e748594d0c0b2e630fdb7b68001a9b
chienleow <chienleow1@gmail.com>
2020-09-25 17:36:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function multiply(num1, num2) {
-    return num1 * num2;
-}
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+var mod = 16 % 6;
+
+var max = Math.max(5, 10, 15, 20);
