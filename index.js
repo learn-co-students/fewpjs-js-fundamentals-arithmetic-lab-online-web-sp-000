85f8cffede3cc78905213b26de94d7e1a3f5ff3c
Emily Gann <aspentree79@gmail.com>
2020-10-04 14:05:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
 // Write your code here
+
+const num1 = 2
+const num2 =31
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 1000) + 1
+const mod = 40 % 6
+const max = Math.max(2, 4, 5, 15, 20)
