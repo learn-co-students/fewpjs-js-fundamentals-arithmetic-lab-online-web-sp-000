a12ed6b0a33fa99cd99220b3d1057027e66c7f5a
carrollm2 <carrollm2@gmail.com>
2020-10-11 10:55:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+function multiply(num1, num2){
+    product = num1 * num2;
+}
+
+function random(){
+    randomNumber = Math.floor(Math.random() * 10) + 1;
+}
+
+function mod(num1, num2){
+    result= num1 % num2; 
+}
+
+function maxNum(num1){
+    result = Math.max(20, num1)
+}
