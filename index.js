f991a780023cc0c5eb974f51d9e0034cd1572929
ShaylaWhite <shayladwhite1@gmail.com>
2020-11-01 21:24:50 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+
+const num1 = 31
+const num2 = 2
+
+const multiply = num1 * num2
+
+const random = Math.floor( Math.random() * 100) + 1
+
+const mod = 16 % 6
+
+const max = Math.max(2, 4, 6, 20, 2, 3)
