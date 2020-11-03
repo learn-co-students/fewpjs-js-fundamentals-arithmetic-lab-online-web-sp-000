3fb3a0019659eb2f3260743569b9b30697635989
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-11-03 06:56:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
+let num1 = 31
+let num2 = 2
 // Write your code here
+let multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10);
+
+let mod = 24 % 5;
+
+let max = Math.max(4, 8, 12, 16, 20);
