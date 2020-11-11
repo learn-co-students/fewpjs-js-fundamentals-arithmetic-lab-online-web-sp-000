1bc4fd202ea76214be18c1053a802ea1a4c8c641
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-11 00:33:23 +0000

    Done.

--- index.js
+++ index.js
@@ -2,5 +2,15 @@
 // Write your code here
 
 // 1. Create a variable called multiply that will multiply the variables num1 and num2 whose results will equal "62".
+const num1 = 2
+const num2 = 31
 var multiply = num1 * num2
-multiply = "62"
+
+// 2. Create a variable called random that will generate a random integer greater than 0.
+var random = Math.floor(Math.random() * 1000) + 1
+
+// 3. Create a variable called mod set to an equation whose remainder is equal to "4".
+var mod = 16 % 6
+
+// 4. Create a variable called max that will return "20" as the highest number in the set.
+var max = Math.max(5, 10, 15, 20)
