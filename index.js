91fe1eb08eb7e47af39fa2c260890ee7fedc6b61
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-11 00:22:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 
 // Write your code here
+
+// 1. Create a variable called multiply that will multiply the variables num1 and num2 whose results will equal "62".
+var multiply = num1 * num2
+multiply = "62"
