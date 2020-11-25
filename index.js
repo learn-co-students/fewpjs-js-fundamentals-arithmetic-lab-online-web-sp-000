84c2cba6b00914b6957b89aa2b070448a73fe3ce
Brennan Roel <brennanroel@gmail.com>
2020-11-25 12:55:50 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2;
+let random = Math.floor( Math.random() * 100 ) + 1;
+let mod = 16 % 6;
+let max = Math.max(1,5,20)
+
