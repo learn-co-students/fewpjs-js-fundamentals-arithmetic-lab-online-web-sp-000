b8bf22dbf4ac3a4669220be6dd5d3df4ca53789e
David Ingram <david@ingramaviation.com>
2020-12-15 12:22:55 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+
+const num1 = 31
+const num2 = 2
+
+let multiply = num1 * num2
+let random = Math.ceil(Math.random() * 100)
+let mod = 10 % 6
+let max = Math.max(1, 4, 15, 20, 16) 
