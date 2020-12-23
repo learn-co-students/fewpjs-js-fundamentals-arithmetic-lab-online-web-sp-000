4c2d6472b5387c707978634a4c60d4ae80adf563
zchesny <z.chesny@gmail.com>
2020-12-23 12:19:42 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+
+const num1 = 31; 
+const num2 = 2; 
+const multiply = num1 * num2; 
+
+const random = Math.ceil(Math.random() * 10)
+
+const mod = 19 % 5;
+
+const max = Math.max(1, 2, 3, 4, 20);
