804e9c48323da5c9fc1d77dc30f80d1b6d0a0762
Drew Thompson <drewallynthompson@gmail.com>
2021-01-12 09:13:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = 19 % 5;
+
+
+let max = Math.max(0, 1, 20);
