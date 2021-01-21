ace17bca8f371bb629a6699a390ef203bcde1f11
Brian Vehige <brianvehige@gmail.com>
2021-01-20 22:29:10 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.random() + 1;
+
+const mod = (59 % 5);
+
+const max = Max.max(2, 9, 15, 17, 18, 20);
