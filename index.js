4e677ef9d20f38b3307bf4b86bbe45ceb77e44a4
learn-co-git <argus.two.2@protonmail.com>
2021-01-22 00:08:49 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10);
+
+let mod = 44 % 10;
+
+let max = Math.max(10, 5, 20);
