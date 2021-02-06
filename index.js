0dac8ef0c50627090c6359c57e48fbe0d5ecc0eb
Gerald Ventura <ventura.c.gerald@gmail.com>
2021-02-06 00:48:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.random() * 10;
+
+let num3 = 14;
+let num4 = 10;
+let mod = num3%num4;
+
+let max = Math.max(0, 5, 10, 15, 20);
