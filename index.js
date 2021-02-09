adba3312bea4d30c94f1fb8b003b24b8144e84ac
anpham148 <anpham148@gmail.com>
2021-02-09 17:36:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let num3 = 16;
+let num4 = 6;
+
+let multiply = num1 * num2;
+
+let random = Math.random();
+
+let mod = num3 % num4;
+
+let max = Math.max(2, 4, 6, 8, 10, 20);
