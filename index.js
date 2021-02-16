95e4058ab645b7c9da3b0f1e23b2bf9d92eb4f83
keelsk <keelsk@gmail.com>
2021-02-16 11:08:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 62; 
+let num2 = 1; 
+let num3 = 9;
+let num4 = 5; 
+
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random());
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 20);
