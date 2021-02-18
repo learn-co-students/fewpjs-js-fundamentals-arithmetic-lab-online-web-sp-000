5817dcf4372e0a3a0983cccabe968624a0ccaf38
Jessica Natovich <jessicanatovich@yahoo.com>
2021-02-18 04:09:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const var1 = 2;
+const var2 = 31;
+const multiply = ( var1 * var2 );
+const random = Math.floor( Math.random() * 1000 ) + 1;
+const mod = 16 % 6;
+const max = Math.max( 5, 10, 15, 20 );
+const min =  Math.min( 5, 10, 15, 20 );
+const var1 max = 2.Math.max( 5, 10, 15, 20 ); 
+const var1 min = 2.Math.min( 5, 10, 15, 20 );
+const var2 max = 31.Math.max( 5, 10, 15, 20 );
+const var2 min = 31.Math.min( 5, 10, 15, 20 );
 
-// Write your code here
+ 
+ 
