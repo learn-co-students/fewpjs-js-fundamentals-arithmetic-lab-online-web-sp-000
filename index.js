9ee020403ccfb27cb491e7b9d5c03982411f33af
Robert Clarke <r.jason.clarke@gmail.com>
2021-03-01 16:48:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+const num1 = 2
+const num2 = 31
+let multiply = num1 * num2;
+let random = Math.random();
+const num3 = 14
+const num4 = 10
+let mod = num3 % num4;
+let max = Math.max(12, 20);
 // Write your code here
