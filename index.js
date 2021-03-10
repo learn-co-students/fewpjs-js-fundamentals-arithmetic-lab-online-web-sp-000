f105a05924c6f55d2d010fd46d56c7e216b42dee
yarvinh <yarvinhhernandez@gmail.com>
2021-03-09 23:29:11 -0500

    Done.

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ let num2 = 31
 let num3 = 12
 let num4 = 3
 let multiply  = num1 * num2
-let randomNumber = Math.random() + 1
+let random = Math.floor(Math.random() + 10) 
 
-let mod = num3 / num4
+
+
+let mod = num3/num4
+let max = Math.max(1, 2, 3, 4, 20)
 // let max
