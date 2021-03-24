47b05818f51544c8e1e07b38be2279ce0024a99c
Sonja Parsell <hello@sparsell.com>
2021-03-23 21:29:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let num3 = 29
+let num4 = 5
+
+let nums = (5, 10, 15, 20)
+
+
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let mod = num3 % num4
+
+let max = Math.max(nums)
+
