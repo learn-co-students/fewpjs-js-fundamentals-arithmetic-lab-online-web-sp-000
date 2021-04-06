c657bcbee9e5065f98debfe0d44423cfe6b62fee
amcmullen9291 <amcmullen9291@gmail.com>
2021-04-06 08:56:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+var num1 = 2;
+var num2 = 31;
+var num3 = 24;
+var num4 = 5;
+
+let numbers = [1, 2, 6, 4, 15 ,20];
+
+let multiply = num1 * num2 ;
+
+let random = (Math.random()*10)+1
+
+let mod = num3%num4;
+
+var max = Math.max(1, 2,7, 6, 14,20);
