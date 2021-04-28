e386010f1bc7d05ea0f7c5538fbdfd83634018fa
Keith Filkins <kafilkins@me.com>
2021-04-28 13:26:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2;
+const num2 = 31;
+const num3 = 20;
+const num4 = 8;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.random() * 10;
+
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 5, 6, 10, 15, 20);
