096f8975ab17ac8394557c6b8b0bdf79163ede48
asb1512 <andrew@olympicmedia.com>
2021-05-18 11:13:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
 // Write your code here
+const num1 = 31
+const num2 = 2
+
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random() * 10) + 1
+
+let mod = 22 % 6
+
+let max = Math.max(15,16,17,18,19,20)
