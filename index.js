f26881b42f0ad3af85bd624b050f36b8e268e0fa
bongofury <jtroutmanman@gmail.com>
2021-05-20 20:08:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 6.2;
+let num2 = 10;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() + 1);
+
+let num3 = 16;
+let num4 = 6;
+let mod = 16 % 6;
+
+let max = Math.max(1, 7, 19, 17, 18, 15, 20, 9, 10);
