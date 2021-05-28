5126acdb4e361a2fdc6806ddaf76fb14af294e24
Benjamin Cheng <bencheng200@gmail.com>
2021-05-28 03:40:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 60
+let num2 = 2
+
+let multiply = num1 + num2
+
+let random = Math.floor(Math.random() * 999)
+
+
+let num3 = 14
+let num4 = 10
+
+let mod = num3%num4
+
+let max = Math.max(1,2,3,20)
