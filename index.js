1abcacaa970ead8d6cb2e9fc1940c8bf884a6e0c
russell-macalintal <russell-macalintal@gmail.com>
2021-06-22 03:26:47 -0400

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const num1 = 31;
 const num2 = 2;
 const multiply = num1 * num2;
 
-const random = Math.random()*10 + 1;
+const random = Math.floor(Math.random()*10) + 1;
 const num3 = 12;
 const num4 = 8;
 const mod = num3 % num4;
